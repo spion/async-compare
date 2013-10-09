e1493985412d32e2193e4fccf1df56ed760fc3be
Petka Antonov <petka_antonov@hotmail.com>
2013-10-09 10:18:24 +0300

    merge same model cpu cores

--- performance.js
+++ performance.js
@@ -13,6 +13,13 @@ function printPlatform() {
     var plat = os.type() + " " + os.release() + " " + os.arch() + "\nNode.JS " + node + "\nV8 " + v8;
     var cpus = os.cpus().map(function(cpu){
         return cpu.model;
+    }).reduce(function(o, model){
+        if( !o[model] ) o[model] = 0;
+        o[model]++;
+        return o;
+    }, {});
+    cpus = Object.keys(cpus).map(function( key ){
+        return key + " \u00d7 " + cpus[key];
     }).join("\n");
     console.log(plat + "\n" + cpus + "\n");
 }
